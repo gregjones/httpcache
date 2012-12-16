110bd421058fb055d4de503824f102345556ca5d
Greg <gj@world.(none)>
2012-12-16 16:35:43 +0000

    initial checking of a caching RoundTripper implementation

---
+++ httpcache.go
@@ -0,0 +1,336 @@
+// Package httpcache provides a http.RoundTripper implementation that works as a 
+// mostly RFC-compliant cache for http responses.
+//
+// It is only suitable for use as a 'private' cache (i.e. for a web-browser or an API-client
+// and not for a shared proxy).
+//
+// 'max-stale' set on a request is not currently respected. (max-age and min-fresh both are)
+package httpcache
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"sync"
+	"time"
+)
+
+var _ = fmt.Println
+
+const (
+	STALE = iota
+	FRESH
+	TRANSPARENT
+	XFromCache = "X-From-Cache"
+)
+
+// A Cache interface is used by the Transport to store and retrieve responses
+type Cache interface {
+	// Get returns the []byte representation of a cached response and a bool
+	// set to true if the value isn't empty
+	Get(key string) (responseBytes []byte, ok bool)
+	// Set stores the []byte representation of a response against a key
+	Set(key string, responseBytes []byte)
+	// Delete removes the value associated with the key
+	Delete(key string)
+}
+
+// MemoryCache is an implemtation of Cache that stores responses in an in-memory map
+type MemoryCache struct {
+	sync.RWMutex
+	items map[string][]byte
+}
+
+func (c *MemoryCache) Get(key string) (resp []byte, ok bool) {
+	c.RLock()
+	defer c.RUnlock()
+	resp, ok = c.items[key]
+	return resp, ok
+}
+
+func (c *MemoryCache) Set(key string, resp []byte) {
+	c.Lock()
+	defer c.Unlock()
+	c.items[key] = resp
+}
+
+func (c *MemoryCache) Delete(key string) {
+	c.Lock()
+	defer c.Unlock()
+	delete(c.items, key)
+}
+
+// NewMemoryCache returns a new Cache that will store items in an in-memory map
+func NewMemoryCache() *MemoryCache {
+	c := &MemoryCache{items: map[string][]byte{}, RWMutex: sync.RWMutex{}}
+	return c
+}
+
+// Transport is an implementation of http.RoundTripper that will return values from a cache
+// where possible (avoiding a network request) and will additionally add validators (etag/if-modified-since)
+// to repeated requests allowing servers to return 304 / Not Modified
+//
+// Note: this means that both the request and response are potentially modified
+type Transport struct {
+	// The RoundTripper interface actually used to make requests
+	// If this follows redirects, then only the final response's cache-control will be taken into account
+	transport http.RoundTripper
+	cache     Cache
+	// If true, responses returned from the cache will be given an extra header, X-From-Cache
+	MarkCachedResponses bool
+}
+
+// NewTransport returns a new Transport using the default HTTP Transport and the
+// provided Cache implementation, with MarkCachedResponses set to true
+func NewTransport(c Cache) *Transport {
+	t := &Transport{transport: http.DefaultTransport, cache: c, MarkCachedResponses: true}
+	return t
+}
+
+// varyMatches will return false unless all of the cached values for the headers listed in Vary
+// match the new request
+func varyMatches(cachedResp *http.Response, req *http.Request) bool {
+	respVarys := cachedResp.Header.Get("vary")
+	for _, header := range strings.Split(respVarys, ",") {
+		header = http.CanonicalHeaderKey(strings.Trim(header, " "))
+		if header != "" && req.Header.Get(header) != cachedResp.Header.Get("X-Varied-"+header) {
+			return false
+		}
+	}
+	return true
+}
+
+// RoundTrip takes a Request and returns a Response
+//
+// If there is a fresh Response already in cache, then it will be returned without connecting to
+// the server.
+//
+// If there is a stale Response, then any validators it contains will be set on the new request
+// to give the server a chance to response with NotModified. If this happens, then the cached Response
+// will be returned.
+func (t *Transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	cacheKey := req.URL.String()
+	cachedVal, _ := t.cache.Get(cacheKey)
+	cacheableMethod := req.Method == "GET" || req.Method == "HEAD"
+	if !cacheableMethod {
+		// Need to invalidate an existing value
+		t.cache.Delete(cacheKey)
+	}
+
+	if cachedVal != nil && cacheableMethod && req.Header.Get("range") == "" {
+		cachedResp, err := responseFromCache(cachedVal, req)
+		if err == nil {
+			if t.MarkCachedResponses {
+				cachedResp.Header.Set(XFromCache, "1")
+			}
+
+			if varyMatches(cachedResp, req) {
+				// Can only use cached value if the new request doesn't Vary significantly
+				freshness := getFreshness(cachedResp.Header, req.Header)
+				if freshness == FRESH {
+					return cachedResp, nil
+				}
+
+				if freshness == STALE {
+					// Add validators if caller hasn't already done so
+					etag := cachedResp.Header.Get("etag")
+					if etag != "" && req.Header.Get("etag") == "" {
+						req.Header.Set("if-none-match", etag)
+					}
+					lastModified := cachedResp.Header.Get("last-modified")
+					if lastModified != "" && req.Header.Get("last-modified") == "" {
+						req.Header.Set("if-modified-since", lastModified)
+					}
+				}
+			}
+
+			resp, err = t.transport.RoundTrip(req)
+			if err == nil && req.Method == "GET" && resp.StatusCode == http.StatusNotModified {
+				// Replace the 304 response with the one from cache, but update with some new headers
+				headersToMerge := getHopByHopHeaders(resp)
+				for _, headerKey := range headersToMerge {
+					cachedResp.Header.Set(headerKey, resp.Header.Get(headerKey))
+				}
+				cachedResp.Status = http.StatusText(http.StatusOK)
+				cachedResp.StatusCode = http.StatusOK
+
+				resp = cachedResp
+			} else {
+				if err != nil || resp.StatusCode != http.StatusOK {
+					t.cache.Delete(cacheKey)
+				}
+			}
+		}
+	} else {
+		reqCacheControl := parseCacheControl(req.Header)
+		if _, ok := reqCacheControl["only-if-cached"]; ok {
+			resp = newGatewayTimeoutResponse(req)
+		} else {
+			resp, err = t.transport.RoundTrip(req)
+		}
+	}
+	reqCacheControl := parseCacheControl(req.Header)
+	respCacheControl := parseCacheControl(resp.Header)
+
+	if canStore(reqCacheControl, respCacheControl) {
+		vary := resp.Header.Get("Vary")
+		for _, varyKey := range strings.Split(vary, ",") {
+			varyKey = http.CanonicalHeaderKey(strings.Trim(varyKey, " "))
+			fakeHeader := "X-Varied-" + varyKey
+			reqValue := req.Header.Get(varyKey)
+			if reqValue != "" {
+				resp.Header.Set(fakeHeader, reqValue)
+			}
+		}
+		respBytes, err := httputil.DumpResponse(resp, true)
+		if err == nil {
+			// fmt.Println("Set cache", string(respBytes))
+			t.cache.Set(cacheKey, respBytes)
+		}
+	} else {
+		t.cache.Delete(cacheKey)
+	}
+	return resp, nil
+}
+
+// getFreshness will return one of Fresh/Stale/Transparent based on the cache-control
+// values of the request and the response
+// 
+// Fresh indicates the response can be returned
+// Stale indicates that the response needs validating before it is returned
+// Transparent indicates the response should not be used to fulfil the request
+//
+// Because this is only a private cache, 'public' and 'private' in cache-control aren't
+// signficant. Similarly, smax-age isn't used.
+//
+// Limitation: max-stale is not taken into account. It should be.
+func getFreshness(respHeaders, reqHeaders http.Header) (freshness int) {
+	respCacheControl := parseCacheControl(respHeaders)
+	reqCacheControl := parseCacheControl(reqHeaders)
+	if _, ok := reqCacheControl["no-cache"]; ok {
+		return TRANSPARENT
+	}
+	if _, ok := respCacheControl["no-cache"]; ok {
+		return STALE
+	}
+	if _, ok := reqCacheControl["only-if-cached"]; ok {
+		return FRESH
+	}
+	dateHeader := respHeaders.Get("date")
+	if dateHeader != "" {
+		date, err := time.Parse(time.RFC1123, dateHeader)
+		if err != nil {
+			return STALE
+		}
+		currentAge := time.Since(date)
+		var lifetime time.Duration
+		zeroDuration, _ := time.ParseDuration("0s")
+		// If a response includes both an Expires header and a max-age directive, 
+		// the max-age directive overrides the Expires header, even if the Expires header is more restrictive.
+		if maxAge, ok := respCacheControl["max-age"]; ok {
+			lifetime, err = time.ParseDuration(maxAge + "s")
+			if err != nil {
+				lifetime = zeroDuration
+			}
+		} else {
+			if expiresHeader, ok := respCacheControl["expires"]; ok {
+				expires, err := time.Parse(time.RFC1123, expiresHeader)
+				if err != nil {
+					lifetime = zeroDuration
+				} else {
+					lifetime = expires.Sub(date)
+				}
+			}
+		}
+
+		if maxAge, ok := reqCacheControl["max-age"]; ok {
+			// the client is willing to accept a response whose age is no greater than the specified time in seconds
+			lifetime, err = time.ParseDuration(maxAge + "s")
+			if err != nil {
+				lifetime = zeroDuration
+			}
+		}
+		if minFresh, ok := reqCacheControl["min-fresh"]; ok {
+			//  the client wants a response that will still be fresh for at least the specified number of seconds.
+			minFreshDuration, err := time.ParseDuration(minFresh + "s")
+			if err != nil {
+				currentAge = time.Duration(currentAge.Nanoseconds() + minFreshDuration.Nanoseconds())
+			}
+		}
+
+		if lifetime > currentAge {
+			return FRESH
+		}
+
+	}
+	return STALE
+}
+
+func getHopByHopHeaders(resp *http.Response) []string {
+	// These headers are always hop-by-hop
+	headers := []string{"connection", "keep-alive", "proxy-authenticate", "proxy-authorization", "te", "trailers", "transfer-encoding", "upgrade"}
+
+	for _, extra := range strings.Split(resp.Header.Get("connection"), ",") {
+		// any header listed in connection, if present, is also considered hop-by-hop
+		if strings.Trim(extra, " ") != "" {
+			headers = append(headers, extra)
+		}
+	}
+	return headers
+}
+
+func canStore(reqCacheControl, respCacheControl cacheControl) (canStore bool) {
+	if _, ok := respCacheControl["no-store"]; ok {
+		return false
+	}
+	if _, ok := reqCacheControl["no-store"]; ok {
+		return false
+	}
+	return true
+}
+
+func responseFromCache(cachedVal []byte, req *http.Request) (*http.Response, error) {
+	b := bytes.NewBuffer(cachedVal)
+	resp, err := http.ReadResponse(bufio.NewReader(b), req)
+	return resp, err
+}
+
+func newGatewayTimeoutResponse(req *http.Request) *http.Response {
+	var braw bytes.Buffer
+	braw.WriteString("HTTP/1.1 504 Gateway Timeout\r\n\r\n")
+	resp, err := http.ReadResponse(bufio.NewReader(&braw), req)
+	if err != nil {
+		panic(err)
+	}
+	return resp
+}
+
+type cacheControl map[string]string
+
+func parseCacheControl(headers http.Header) cacheControl {
+	cc := cacheControl{}
+	ccHeader := headers.Get("Cache-Control")
+	for _, part := range strings.Split(ccHeader, ",") {
+		part = strings.Trim(part, " ")
+		if part == "" {
+			continue
+		}
+		if strings.ContainsRune(part, '=') {
+			keyval := strings.Split(part, "=")
+			cc[strings.Trim(keyval[0], " ")] = strings.Trim(keyval[1], ",")
+		} else {
+			cc[part] = "1"
+		}
+	}
+	return cc
+}
+
+// NewMemoryCacheTransport returns a new Transport using the in-memory cache implementation
+func NewMemoryCacheTransport() *Transport {
+	c := NewMemoryCache()
+	t := NewTransport(c)
+	return t
+}
---
+++ httpcache_test.go
@@ -0,0 +1,253 @@
+package httpcache
+
+import (
+	"fmt"
+	. "launchpad.net/gocheck"
+	"net"
+	"net/http"
+	"testing"
+)
+
+var _ = fmt.Print
+
+func Test(t *testing.T) { TestingT(t) }
+
+type S struct {
+	listener  net.Listener
+	client    http.Client
+	transport *Transport
+}
+
+var _ = Suite(&S{})
+
+func (s *S) SetUpSuite(c *C) {
+	t := NewMemoryCacheTransport()
+	client := http.Client{Transport: t}
+	s.transport = t
+	s.client = client
+
+	ln, err := net.Listen("tcp", ":9090")
+	if err != nil {
+		panic(err)
+	}
+	s.listener = ln
+
+	http.HandleFunc("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "max-age=3600")
+	}))
+
+	http.HandleFunc("/nostore", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store")
+	}))
+
+	http.HandleFunc("/etag", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		etag := "124567"
+		if r.Header.Get("if-none-match") == etag {
+			w.WriteHeader(http.StatusNotModified)
+		}
+		w.Header().Set("etag", etag)
+	}))
+
+	http.HandleFunc("/lastmodified", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		lm := "Fri, 14 Dec 2010 01:01:50 GMT"
+		if r.Header.Get("if-modified-since") == lm {
+			w.WriteHeader(http.StatusNotModified)
+		}
+		w.Header().Set("last-modified", lm)
+	}))
+
+	http.HandleFunc("/varyaccept", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "max-age=3600")
+		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Vary", "Accept")
+		w.Write([]byte("Some text content"))
+	}))
+
+	http.HandleFunc("/doublevary", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "max-age=3600")
+		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Vary", "Accept, Accept-Language")
+		w.Write([]byte("Some text content"))
+	}))
+	http.HandleFunc("/varyunused", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "max-age=3600")
+		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Vary", "X-Madeup-Header")
+		w.Write([]byte("Some text content"))
+	}))
+
+	go http.Serve(s.listener, nil)
+}
+
+func (s *S) TearDownSuite(c *C) {
+	err := s.listener.Close()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *S) TearDownTest(c *C) {
+	s.transport.cache = NewMemoryCache()
+}
+
+func (s *S) TestGetOnlyIfCachedHit(c *C) {
+	req, err := http.NewRequest("GET", "http://localhost:9090/", nil)
+	c.Assert(err, IsNil)
+	resp, err := s.client.Do(req)
+	defer resp.Body.Close()
+	c.Assert(resp.Header.Get(XFromCache), Equals, "")
+
+	req2, err2 := http.NewRequest("GET", "http://localhost:9090/", nil)
+	req2.Header.Add("cache-control", "only-if-cached")
+	resp2, err2 := s.client.Do(req)
+	defer resp2.Body.Close()
+	c.Assert(err2, IsNil)
+	c.Assert(resp2.Header.Get(XFromCache), Equals, "1")
+	c.Assert(resp2.StatusCode, Equals, 200)
+}
+
+func (s *S) TestGetOnlyIfCachedMiss(c *C) {
+	req, err := http.NewRequest("GET", "http://localhost:9090/", nil)
+	req.Header.Add("cache-control", "only-if-cached")
+	resp, err := s.client.Do(req)
+	defer resp.Body.Close()
+	c.Assert(err, IsNil)
+	c.Assert(resp.Header.Get(XFromCache), Equals, "")
+	c.Assert(resp.StatusCode, Equals, 504)
+}
+
+func (s *S) TestGetNoStoreRequest(c *C) {
+	req, err := http.NewRequest("GET", "http://localhost:9090/", nil)
+	req.Header.Add("Cache-Control", "no-store")
+	resp, err := s.client.Do(req)
+	defer resp.Body.Close()
+	c.Assert(err, IsNil)
+	c.Assert(resp.Header.Get(XFromCache), Equals, "")
+
+	resp2, err2 := s.client.Do(req)
+	defer resp2.Body.Close()
+	c.Assert(err2, IsNil)
+	c.Assert(resp2.Header.Get(XFromCache), Equals, "")
+}
+
+func (s *S) TestGetNoStoreResponse(c *C) {
+	req, err := http.NewRequest("GET", "http://localhost:9090/nostore", nil)
+	resp, err := s.client.Do(req)
+	defer resp.Body.Close()
+	c.Assert(err, IsNil)
+	c.Assert(resp.Header.Get(XFromCache), Equals, "")
+
+	resp2, err2 := s.client.Do(req)
+	defer resp2.Body.Close()
+	c.Assert(err2, IsNil)
+	c.Assert(resp2.Header.Get(XFromCache), Equals, "")
+}
+
+func (s *S) TestGetWithEtag(c *C) {
+	req, err := http.NewRequest("GET", "http://localhost:9090/etag", nil)
+	resp, err := s.client.Do(req)
+	defer resp.Body.Close()
+	c.Assert(err, IsNil)
+	c.Assert(resp.Header.Get(XFromCache), Equals, "")
+
+	resp2, err2 := s.client.Do(req)
+	defer resp2.Body.Close()
+	c.Assert(err2, IsNil)
+	c.Assert(resp2.Header.Get(XFromCache), Equals, "1")
+}
+
+func (s *S) TestGetWithLastModified(c *C) {
+	req, err := http.NewRequest("GET", "http://localhost:9090/lastmodified", nil)
+	resp, err := s.client.Do(req)
+	defer resp.Body.Close()
+	c.Assert(err, IsNil)
+	c.Assert(resp.Header.Get(XFromCache), Equals, "")
+
+	resp2, err2 := s.client.Do(req)
+	defer resp2.Body.Close()
+	c.Assert(err2, IsNil)
+	c.Assert(resp2.Header.Get(XFromCache), Equals, "1")
+}
+
+func (s *S) TestGetWithVary(c *C) {
+	req, err := http.NewRequest("GET", "http://localhost:9090/varyaccept", nil)
+	req.Header.Set("Accept", "text/plain")
+	resp, err := s.client.Do(req)
+	defer resp.Body.Close()
+	c.Assert(err, IsNil)
+	c.Assert(resp.Header.Get("Vary"), Equals, "Accept")
+
+	resp2, err2 := s.client.Do(req)
+	defer resp2.Body.Close()
+	c.Assert(err2, IsNil)
+	c.Assert(resp2.Header.Get(XFromCache), Equals, "1")
+
+	req.Header.Set("Accept", "text/html")
+	resp3, err3 := s.client.Do(req)
+	defer resp3.Body.Close()
+	c.Assert(err3, IsNil)
+	c.Assert(resp3.Header.Get(XFromCache), Equals, "")
+
+	req.Header.Set("Accept", "")
+	resp4, err4 := s.client.Do(req)
+	defer resp4.Body.Close()
+	c.Assert(err4, IsNil)
+	c.Assert(resp4.Header.Get(XFromCache), Equals, "")
+}
+
+func (s *S) TestGetWithDoubleVary(c *C) {
+	req, err := http.NewRequest("GET", "http://localhost:9090/doublevary", nil)
+	req.Header.Set("Accept", "text/plain")
+	req.Header.Set("Accept-Language", "da, en-gb;q=0.8, en;q=0.7")
+	resp, err := s.client.Do(req)
+	defer resp.Body.Close()
+	c.Assert(err, IsNil)
+	c.Assert(resp.Header.Get("Vary"), Not(Equals), "")
+
+	resp2, err2 := s.client.Do(req)
+	defer resp2.Body.Close()
+	c.Assert(err2, IsNil)
+	c.Assert(resp2.Header.Get(XFromCache), Equals, "1")
+
+	req.Header.Set("Accept-Language", "")
+	resp3, err3 := s.client.Do(req)
+	defer resp3.Body.Close()
+	c.Assert(err3, IsNil)
+	c.Assert(resp3.Header.Get(XFromCache), Equals, "")
+
+	req.Header.Set("Accept-Language", "da")
+	resp4, err4 := s.client.Do(req)
+	defer resp4.Body.Close()
+	c.Assert(err4, IsNil)
+	c.Assert(resp4.Header.Get(XFromCache), Equals, "")
+}
+
+func (s *S) TestGetVaryUnused(c *C) {
+	req, err := http.NewRequest("GET", "http://localhost:9090/varyunused", nil)
+	req.Header.Set("Accept", "text/plain")
+	resp, err := s.client.Do(req)
+	defer resp.Body.Close()
+	c.Assert(err, IsNil)
+	c.Assert(resp.Header.Get("Vary"), Not(Equals), "")
+
+	resp2, err2 := s.client.Do(req)
+	defer resp2.Body.Close()
+	c.Assert(err2, IsNil)
+	c.Assert(resp2.Header.Get(XFromCache), Equals, "1")
+}
+
+func (s *S) TestParseCacheControl(c *C) {
+	h := http.Header{}
+	for _ = range parseCacheControl(h) {
+		c.Fatal("cacheControl should be empty")
+	}
+
+	h.Set("cache-control", "no-cache")
+	cc := parseCacheControl(h)
+	c.Assert(cc["no-cache"], Equals, "1")
+
+	h.Set("cache-control", "no-cache, max-age=3600")
+	cc = parseCacheControl(h)
+	c.Assert(cc["no-cache"], Equals, "1")
+	c.Assert(cc["max-age"], Equals, "3600")
+}
